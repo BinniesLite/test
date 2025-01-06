e3418c8eb8a73e6013873d9979ac47e53bba06e4
BinniesLite <89041460+BinniesLite@users.noreply.github.com>
2025-01-06 21:34:33 +0700

    do it

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,22 +1,3 @@
-// import type { Metadata } from "next";
-// import { DM_Sans } from "next/font/google";
-// import "./globals.css";
-// import Providers from "./providers";
-// import { ClerkProvider } from "@clerk/nextjs";
-// import { Toaster } from "sonner";
-// import { Suspense } from "react";
-
-// const dmSans = DM_Sans({
-//   subsets: ["latin"],
-//   display: "swap",
-//   variable: "--font-dm-sans",
-// });
-
-// export const metadata: Metadata = {
-//   title: "Educated",
-//   description: "I am free",
-// };
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -25,21 +6,13 @@ export default function RootLayout({
   console.log("Hello world");
   console.log(process.env.NEXT_PUBLIC_API_BASE_URL);
   console.log(process.env.CLERK_SECRET_KEY);
+  
   return (
-    <>
-      Hello world
-    </>
-    // <ClerkProvider>
-    //   <html lang="en">
-    //     <body className={`${dmSans.className}`}>
-    //       <Providers>
-    //         <Suspense fallback={null}>
-    //           <div className="root-layout">{children}</div>
-    //         </Suspense>
-    //         <Toaster richColors closeButton />
-    //       </Providers>
-    //     </body>
-    //   </html>
-    // </ClerkProvider>
+    <html lang="en">
+      <body>
+        Hello world
+        {children}
+      </body>
+    </html>
   );
 }
